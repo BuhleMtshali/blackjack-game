52903cdf71e2d4c110845f99b57b863f9e603b4d
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-09 21:36:22 +0200

    updated the js file

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 let messageElement = document.querySelector("#message-el");
 let sumElement = document.querySelector("#sum-el");
 let cardsElement = document.querySelector("#cards-el");
-let sum = document.querySelector("#sum-el");
+let sum = 0;
 let score = document.querySelector("#score");
+let message = "";
+score = 200;
+let hasBlackJack = false;
+let isAlive = false;
+let cards = [];
 
-function startGame() {
+function getRandomCard(event) {
+  event.preventDefault();
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame(event) {
+  event.preventDefault();
+  isAlive = true;
+  let firstcard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstcard, secondCard];
+  sum = firstcard + secondCard;
+
   renderGame();
 }
 
@@ -13,6 +37,6 @@ function renderGame(event) {
 }
 
 let startGameButton = document.querySelector("#start-game");
-startGameButton.addEventListener("click", startGame);
 let newCardButton = document.querySelector("#new-card");
+startGameButton.addEventListener("click", startGame);
 newCardButton.addEventListener("click", newCard);
